b2d3d3931b04d45b7b56876302a8b4799c2344bd
feranmi <feranmiolasunkanmi91@gmail.com>
2024-05-16 13:57:32 +0100

    fix on tenancy

--- config/tenancy.php
+++ config/tenancy.php
@@ -17,8 +17,10 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        '127.0.0.1',
-        'localhost',
+        // '127.0.0.1',
+        // 'localhost',
+        getenv("CENTRAL_DOMAIN1"),
+        getenv("CENTRAL_DOMAIN2"),
     ],
 
     /**
